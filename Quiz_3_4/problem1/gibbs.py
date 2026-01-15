8c3665adf4f40a84c4ae3ab13fe9900d901eea12
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2026-01-15 11:12:07 +0400

    hw1

--- Quiz_3_4/problem1/gibbs.py
+++ Quiz_3_4/problem1/gibbs.py
@@ -3,33 +3,69 @@ import sys
 import string
 import random
 
-#### INSTRUCTIONS FOR USE:
-# call program as follows: ./gibbs.py <Motif Length> <Data File>
-# make sure the gibbs.py is marked as executable: chmod +x gibbs.py
-
 alphabet = ['A', 'G', 'C', 'T']
 
-#### GibbsSampler:
-#### 	INPUTS:	S - list of sequences
-####		L - length of motif
-####	OUTPUT:	PWM - 4xL list with frequencies of each base at each position
-####                  Order of bases should be consistent with alphabet variable
-def GibbsSampler(S,L):
-    PWM = []
-    for i in range(len(alphabet)):
-        PWM.append([0]*L)
+def build_pwm(S, positions, L, exclude_idx):
+    counts = [[1 for _ in range(L)] for _ in range(len(alphabet))]
+    
+    for i, seq in enumerate(S):
+        if i == exclude_idx:
+            continue
+        pos = positions[i]
+        motif = seq[pos:pos+L]
+        for j, base in enumerate(motif):
+            if base in alphabet:
+                counts[alphabet.index(base)][j] += 1
+    
+    pwm = [[0.0 for _ in range(L)] for _ in range(len(alphabet))]
+    for j in range(L):
+        col_sum = sum(counts[k][j] for k in range(len(alphabet)))
+        for k in range(len(alphabet)):
+            pwm[k][j] = float(counts[k][j]) / col_sum
+    
+    return pwm
 
-    ######### ADD YOUR CODE HERE ######
+def score_kmer(kmer, pwm):
+    score = 1.0
+    for i, base in enumerate(kmer):
+        if base in alphabet:
+            score *= pwm[alphabet.index(base)][i]
+    return score
 
-    ######### END OF YOUR CODE HERE #####
-	
-    return PWM
+def sample_position(seq, L, pwm):
+    scores = []
+    for i in range(len(seq) - L + 1):
+        kmer = seq[i:i+L]
+        scores.append(score_kmer(kmer, pwm))
+    
+    total = sum(scores)
+    if total == 0:
+        return random.randint(0, len(seq) - L)
+    
+    probs = [s/total for s in scores]
+    rand_val = random.random()
+    cumsum = 0
+    for i, p in enumerate(probs):
+        cumsum += p
+        if rand_val <= cumsum:
+            return i
+    return len(seq) - L
 
-###### YOUR OWN FUNCTIONS HERE
-# optional -- feel free to add your own functions if you want to
-
-
-###### END OF YOUR FUNCTIONS
+def GibbsSampler(S, L):
+    positions = [random.randint(0, len(seq) - L) for seq in S]
+    
+    iterations = 1000
+    for iteration in range(iterations):
+        exclude_idx = random.randint(0, len(S) - 1)
+        
+        pwm = build_pwm(S, positions, L, exclude_idx)
+        
+        new_pos = sample_position(S[exclude_idx], L, pwm)
+        positions[exclude_idx] = new_pos
+    
+    final_pwm = build_pwm(S, positions, L, -1)
+    
+    return final_pwm
 
 def main():
     L = int(sys.argv[1])
